c1a1a3b6e25bc7dace3164642f7520bdf92816cf
shabawak <shabawak@gmai.com>
2023-07-17 21:01:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line of text using the puts function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
